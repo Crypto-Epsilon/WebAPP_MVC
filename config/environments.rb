2d92ce877d4cd6543d8a26e6a00d04fae01f6298
Malherbe <malherbe.billy@gmail.com>
2021-05-20 14:16:36 +0800

    New Web Application

---
+++ config/environments.rb
@@ -0,0 +1,31 @@
+require 'roda'
+require 'figaro'
+require 'logger'
+
+module Pets_Tinder
+  # Configuration for the API
+  class App < Roda
+    plugin :environments
+
+    # Environment variables setup
+    Figaro.application = Figaro::Application.new(
+      environment: environment,
+      path: File.expand_path('config/secrets.yml')
+    )
+    Figaro.load
+    def self.config() = Figaro.env
+
+    # Logger setup
+    LOGGER = Logger.new($stderr)
+    def self.logger() = LOGGER
+
+    configure :development, :test do
+      require 'pry'
+
+      # Allows running reload! in pry to restart entire app
+      def self.reload!
+        exec 'pry -r ./spec/test_load_all'
+      end
+    end
+  end
+end
---
+++ spec/test_load_all.rb
@@ -0,0 +1,10 @@
+require_relative '../require_app'
+require_app
+
+# run pry -r <path/to/this/file>
+require 'rack/test'
+include Rack::Test::Methods # rubocop:disable Style/MixinUsage
+
+def app
+  Pets_Tinder::App
+end
