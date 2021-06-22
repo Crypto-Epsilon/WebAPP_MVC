3b8cf8bfc1de2296ce46effc60a882ab2e6f1b2d
Ed <edgar.duronr@gmail.com>
2021-06-22 20:33:37 +0800

    fixing CurrentSession (auth scopes and SSo)

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -39,9 +39,9 @@ module Pets_Tinder
             authenticated[:auth_token]
           )
 
-          CurrentSession.new(session).set(:current_account, account)
+          CurrentSession.new(session).current_account = current_account
 
-          flash[:notice] = "Welcome back #{account['username']}!"
+          flash[:notice] = "Welcome back #{current_account.username}!"
           routing.redirect '/pets'
         rescue AuthenticateAccount::NotAuthenticatedError
           flash[:error] = 'Username and password did not match our records'
