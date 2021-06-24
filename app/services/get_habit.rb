78d2fbf08c46e2c089078fe475bd4e8f3debd03c
Ed <edgar.duronr@gmail.com>
2021-06-24 21:32:36 +0800

    fixing get_habit.rb

---
+++ app/services/get_habit.rb
@@ -0,0 +1,15 @@
+require 'http'
+
+# Returns all projects belonging to an account
+class GetHabit
+  def initialize(config)
+    @config = config
+  end
+
+  def call(user, hab_id)
+    response = HTTP.auth("Bearer #{user.auth_token}")
+                   .get("#{@config.API_URL}/documents/#{hab_id}")
+
+    response.code == 200 ? JSON.parse(response.body.to_s)['data'] : nil
+  end
+end
